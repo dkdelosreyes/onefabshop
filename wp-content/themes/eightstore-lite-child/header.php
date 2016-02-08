d275ea1b4088ea221f878f9f05460c9a852470a0
dkdelosreyes <diannekatherinedelosreyes@gmail.com>
2016-02-08 23:35:58 +0800

    homepage colors changes, integrated facebook page plugin

--- wp-content/themes/eightstore-lite-child/header.php
+++ wp-content/themes/eightstore-lite-child/header.php
@@ -19,6 +19,18 @@
 </head>
 
 <body <?php body_class(); ?>>
+
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=855665087877829";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
 	<div id="page" class="hfeed site">
 		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'eightstore-lite' ); ?></a>
 
