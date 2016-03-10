cec002068c5629fbfdc8beee1fcad7ad026b17e9
dianne delosreyes <https://diannedelosreyes@bitbucket.org>
2016-03-10 11:58:55 +0800

    header font style changes, gitignore

--- wp-content/themes/eightstore-lite-child/header.php
+++ wp-content/themes/eightstore-lite-child/header.php
@@ -14,7 +14,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+	<!-- <link href='https://fonts.googleapis.com/css?family=Alex+Brush|Yellowtail|Leckerli+One|Lobster|Pacifico|Qwigley|Mountains+of+Christmas:400,700' rel='stylesheet' type='text/css'> -->
+	<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
 	<?php wp_head(); ?>
 </head>
 
