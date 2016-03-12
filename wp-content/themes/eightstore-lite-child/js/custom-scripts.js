86e5dabab6c73fe9f2fb9652cae4a2c44c10a8ea
Dianne Katherine Delos Reyes <diannekatherinedelosreyes@Diannes-MacBook-Pro.local>
2016-03-12 22:31:58 +0800

    view cart styles and sticky menu bar jquery

--- wp-content/themes/eightstore-lite-child/js/custom-scripts.js
+++ wp-content/themes/eightstore-lite-child/js/custom-scripts.js
@@ -1,3 +1,16 @@
-jQuery('.item-prod-wrap').matchHeight({
-                        property: 'min-height'
-                    });
+jQuery(document).ready(function() {
+
+	jQuery('.item-prod-wrap').matchHeight({
+	    property: 'min-height'
+	});
+
+	jQuery(window).scroll(function () {
+	      // console.log(jQuery(window).scrollTop())
+	    if (jQuery(window).scrollTop() > 220) {
+	      jQuery('.store-menu').addClass('store-menu-sticky');
+	    }
+	    if (jQuery(window).scrollTop() < 220) {
+	      jQuery('.store-menu').removeClass('store-menu-sticky');
+	    }
+	});
+});
